ba677d8018e56d084279a342049dcb7c3033ae73
prabhutejad <prabhuteja.iot@gmail.com>
2024-08-11 14:56:43 +0530

    Components added

---
+++ public/src/app/features/socialevents/socialevents.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SocialeventsComponent } from './socialevents.component';
+
+describe('SocialeventsComponent', () => {
+  let component: SocialeventsComponent;
+  let fixture: ComponentFixture<SocialeventsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SocialeventsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SocialeventsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
